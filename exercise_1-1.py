1c66ef9cd567fb788bf693277cd6f78b83e5d4a7
Cole McMullen <coledmcmullen@gmail.com>
2021-11-12 20:26:16 -0500

    initial comments

--- exercise_1-1.py
+++ exercise_1-1.py
@@ -1,5 +1,10 @@
+# Cole McMullen
+# Programming Project 1
+# Exercise 1
+
 import math
 
+# apply the given function to an input
 def function(x):
 	a = 230
 	b = 18
@@ -9,6 +14,7 @@ def function(x):
 
 	return (a*math.pow(x, 4)) + (b*math.pow(x, 3)) + (c*math.pow(x, 2)) + (d*x) + e
 
+# apply the derivative of the function to an input
 def function_prime(x):
 	a = 230
 	b = 18
@@ -17,6 +23,7 @@ def function_prime(x):
 
 	return (a*4*math.pow(x, 3)) + (b*3*math.pow(x, 2)) + (c*2*x) + d
 
+# apply the secant method to a function given two starting points and a tolerance
 def secant_method(func, x_k, x_ksub1, err):
 	f_x_k = func(x_k)
 	f_x_ksub1 = func(x_ksub1)
@@ -27,6 +34,7 @@ def secant_method(func, x_k, x_ksub1, err):
 	x_kplus1 = x_k - f_x_k/((f_x_k - f_x_ksub1)/(x_k - x_ksub1))
 	return secant_method(func, x_kplus1, x_k, err)
 
+# apply newton's method to a function given its derivative, a starting point and a tolerance
 def newton_method(func, func_prime, x, err):
 	f_x = func(x)
 	f_prime_x = func_prime(x)
@@ -35,13 +43,15 @@ def newton_method(func, func_prime, x, err):
 	new_x = x - (f_x/f_prime_x)
 	return newton_method(func, func_prime, new_x, err)
 
+# apply the methods to the desired intervals
 print("Finding result in [-1, 0] using secant method...")
 result = secant_method(function, -1, 0, math.pow(10, -6))
 print(result)
 
-# is there a way to make this work by changing my code?
+# just providing 0 and 1 as starting values finds the result in
+# [-1, 0], so closer starting points have to be used
 print("Finding result in [0, 1] using secant method...")
-result = secant_method(function, 0.8, 1.2, math.pow(10, -6))
+result = secant_method(function, 0.8, 1, math.pow(10, -6))
 print(result)
 
 print("Finding result in [-1, 0] using newton method...")
--- exercise_1-2.py
+++ exercise_1-2.py
@@ -1,12 +1,18 @@
+# Cole McMullen
+# Programming Project 1
+# Excercise 2
+
 import math
 import numpy as np
 
+# generate a matrix A for a given value of epsilon
 def A_gen(epsilon):
 	return np.array([[2, -2, 0],
 			 [epsilon - 2, 2, 0],
 			 [0, -1, 3]])
 
 
+# generate the LU decomposition of a 3x3 matrix
 def LU(arr):
 	for i in range (0, 2):
 		U[0, i] = arr[0, i]
@@ -16,9 +22,12 @@ def LU(arr):
 	L[2, 1] = (arr[2, 1] - (L[2, 0]*U[0, 1]))/U[1, 1]
 	U[2, 2] = (arr[2, 2] - (L[2, 0]*U[0, 2] + L[2, 1]*U[1, 2]))/L[2, 2]
 
+# set the expected vector x to [1, 1, 1]
 xex = np.ones(3)
 print("X_exact = ")
 print(xex)
+# solve for x given b = [0, epsilon, 2] and find the error
+# using epsilon = 10^-k, k = 0, 1, ..., 9
 for k in range (0, 10):
 	U = np.zeros((3, 3))
 	L = np.eye(3)
@@ -33,6 +42,9 @@ for k in range (0, 10):
 	print("Error: ")
 	print(x - xex)
 
+# solve for x given b = [2log(5/2) - 2, (epsilon - 2)log(5/2) + 2, 2]
+# and find the error
+# using epsilon = 1/3 * 10^-k, k = 0, 1, ..., 9
 xex = np.array([math.log(5/2), 1, 1]).T
 print("\n\nX_exact = ")
 print(xex)
